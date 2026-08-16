065acc42b7b3b3b64eed12ce5e3224cb8ab723b9
Sakai Hayate <future4219@yahoo.co.jp>
2026-08-16 23:01:26 +0900

    use system font stack

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/ibm-plex-sans-jp/japanese-400.css";
-import "@fontsource/ibm-plex-sans-jp/latin-400.css";
-import "@fontsource/ibm-plex-sans-jp/japanese-600.css";
-import "@fontsource/ibm-plex-sans-jp/latin-600.css";
-import "@fontsource/ibm-plex-mono/latin-400.css";
-import "@fontsource/ibm-plex-mono/latin-500.css";
-import "@fontsource/ibm-plex-mono/latin-600.css";
 import App from "./App";
 import "./styles.css";
 
